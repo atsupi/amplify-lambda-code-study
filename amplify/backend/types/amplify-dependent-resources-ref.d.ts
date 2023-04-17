344f6c23c467e70f3fbd4c0642731aadb2ccc6eb
atsupi <atsupi@gmail.com>
2023-04-18 00:05:05 +0900

    Initial commit.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifylambdacodestu76a2d1ef": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger25d0c812": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "lambdacodestudy": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
